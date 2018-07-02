5ebceeb4d74b9efc9c2a3ac07f452e3060756742
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-07-02 14:00:18 -0400

    Added Judge Data

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gavel from './Assets/gavel.png';
 import { getFutureDate } from './randomizer';
+import Judges from './data/judgeData';
 
 import './App.css';
 
@@ -8,6 +9,8 @@ class App extends Component {
   state = {
     spinning: false,
     date: getFutureDate(2018, 2030),
+    congress: "Republican",
+    presidency: "Republican"
   }
 
   toggleSpin() {
@@ -15,12 +18,23 @@ class App extends Component {
   }
 
   render() {
+    console.log(Judges);
     const spinning = this.state.spinning;
     return (
       <div>
         <h1>Supreme Court Roulette!</h1>
         {spinning && <img src={gavel} alt="gavel" className="spinning" />}
         {!spinning && <img src={gavel} alt="gavel" className="notspinning" />}
+          <div className="judge-container">
+            {Judges.CurrentJustices.map(judge => {
+              return (
+                <div className="judge">
+                  <img src={judge.picture} height="200" />
+                    <p>{judge.name}</p>
+                </div>
+            )
+            })}
+          </div>
         <button onClick={() => this.toggleSpin()}>Spin!</button>
       </div>
     );
---
+++ react-frontend/src/data/judgeData.js
@@ -0,0 +1,167 @@
+const Judges = {
+  CurrentJustices: [
+    {
+      name: "John Roberts",
+      position: "Chief Justice",
+      date_dob: "1955-01-27",
+      id: 2738, 
+      party: "r",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Official_roberts_CJ.jpg/220px-Official_roberts_CJ.jpg"      
+    },
+    {
+      name: "Anthony Kennedy",
+      position: "Associate Justice",
+      date_dob: "1936-01-01",
+      id: 1747,
+      party: "r",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Anthony_Kennedy_official_SCOTUS_portrait.jpg/220px-Anthony_Kennedy_official_SCOTUS_portrait.jpg"      
+    },
+    {
+      name: "Clarence Thomas",
+      position: "Associate Justice",
+      date_dob: "1948-06-23",
+      id: 3200, 
+      party: "r",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Clarence_Thomas_official_SCOTUS_portrait.jpg/220px-Clarence_Thomas_official_SCOTUS_portrait.jpg"      
+    },
+    {
+      name: "Ruth Bader Ginsburg",
+      position: "Associate Justice",
+      date_dob: "1933-01-01",
+      id: 1213,
+      party: "d",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Ruth_Bader_Ginsburg_2016_portrait.jpg/220px-Ruth_Bader_Ginsburg_2016_portrait.jpg"
+    },
+    {
+      name: "Stephen Breyer",
+      position: "Associate Justice",
+      date_dob: "1938-01-01",
+      id: 384,
+      party: "d",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Stephen_Breyer%2C_SCOTUS_photo_portrait.jpg/220px-Stephen_Breyer%2C_SCOTUS_photo_portrait.jpg"
+    },
+    {
+      name: "Samuel Anthony Alito Jr.",
+      position: "Associate Justice",
+      date_dob: "1950-01-01",
+      id: 77,
+      party: "r",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Samuel_Alito_official_photo.jpg/220px-Samuel_Alito_official_photo.jpg"      
+    },
+    {
+      name: "Sonia Maria Sotomayor",
+      position: "Associate Justice",
+      date_dob: "1954-01-01",
+      id: 3045,
+      party: "d",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Sonia_Sotomayor_in_SCOTUS_robe.jpg/1920px-Sonia_Sotomayor_in_SCOTUS_robe.jpg"
+    },
+    {
+      name: "Elena Kagan",
+      position: "Associate Justice",
+      date_dob: "1960-04-28",
+      id: 1691,
+      party: "d",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Elena_Kagan_Official_SCOTUS_Portrait_%282013%29.jpg/1280px-Elena_Kagan_Official_SCOTUS_Portrait_%282013%29.jpg"
+    },
+    {
+      name: "Neil McGill Gorsuch",
+      position: "Associate Justice",
+      date_dob: "1967-08-29",
+      id: 1250,
+      party: "r",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/9/97/Associate_Justice_Neil_Gorsuch_Official_Portrait.jpg"
+    },
+  ],
+  DemJudges: [
+    {
+      name: "Srikanth Srinivasan",
+      id: 3065,
+      picture: "https://en.wikipedia.org/wiki/File:Sri_Srinavasan.jpg"
+    },
+    {
+      name: "Paul Jeffrey Watford",
+      id: 3385,
+      picture: "https://en.wikipedia.org/wiki/File:Judge_Paul_J._Watford.jpg"
+    },
+    {
+      name: "Patricia Ann Millett",
+      id: 2263,
+      picture: "https://api.ballotpedia.org/v3/thumbnail/200/300/crop/best/Patricia_Ann_Millett_photo_2016.jpg" 
+    },
+    {
+      name: "Merrick B. Garland",
+      id: 1155,
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Merrick_Garland.jpg/220px-Merrick_Garland.jpg"
+    },
+    {
+      name: "Robert Leon Wilkins",
+      id: 3473,
+      picture: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiG97Td-4DcAhUPXK0KHXbhDFYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.rose-hulman.edu%2Fnews%2F2017%2Ffederal-judge-robert-l-wilkins-to-present-rhits-commencement-address.html&psig=AOvVaw1fum9dJ694Bk60tzlZvP9O&ust=1530639794353200"
+    },
+    {
+      name: "Jane Louise Kelly",
+      id: 1738,
+      picture: "https://www.google.com/imgres?imgurl=https://www.afj.org/wp-content/uploads/2013/09/Jane-Kelly-e1457014832978-239x300.jpg&imgrefurl=https://www.afj.org/our-work/nominees/jane-l-kelly&h=300&w=239&tbnid=Rz8nGp_wMAPO1M:&q=Jane+L.+Kelly&tbnh=160&tbnw=126&usg=__sCW4UPoxwp3Gvy-Rf36ESgSlN-A%3D&vet=1&docid=HYbG7TldkRZyIM&itg=1&hl=en&sa=X&ved=0ahUKEwic4L6C_IDcAhUPYK0KHUVXCqEQ_B0InQEwCg"
+    },
+    {
+      name: "Paul Jeffrey Watford",
+      id: 3385,
+      picture: "https://alchetron.com/cdn/paul-j-watford-40663b59-3bbb-42b5-950c-1e4af870e74-resize-750.jpeg"
+    },
+    {
+      name: "Jacqueline Nguyen",
+      id: 2403,
+      picture: "https://www.japantimes.co.jp/wp-content/uploads/2016/03/f-nguyen-a-20160307.jpg"
+    },
+    {
+      name: "Patricia Ann Millett",
+      id: 2263,
+      picture: "https://www.ali.org/media/filer_public_thumbnails/filer_public/4e/ec/4eec6843-8f7c-47b0-a1d1-5070d30b2b72/millett-particia.jpg__310x393_q85_crop_subject_location-159,159_subsampling-2_upscale.jpg"
+    }
+  ],
+  RepJudges: [
+    {
+      name: "Brett Kavanaugh",
+      id: 1713,
+      picture: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Judge_Brett_Kavanaugh.jpg"
+    },
+    {
+      name: "Raymond Gruender",
+      id: 1299,
+      picture: "http://www.scotusblog.com/wp-content/uploads/2017/01/JudgeGruender2.jpg"
+    },
+    {
+      name: "Thomas Hardiman",
+      id: 1363,
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/JudgeThomasHardiman.pdf/page1-958px-JudgeThomasHardiman.pdf.jpg"
+    },
+    {
+      name: "Raymond Kethledge",
+      id: 1765,
+      picture: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Judge_Raymond_Kethledge.png"
+    },
+    {
+      name: "William H. Pryor Jr.",
+      id: 2636,
+      picture: "https://upload.wikimedia.org/wikipedia/commons/a/ac/Portrait_of_US_federal_judge_William_H._Pryor%2C_Jr.jpg"
+    },
+    {
+      name: "Amul Thapar",
+      id: 3195,
+      picture: "https://upload.wikimedia.org/wikipedia/commons/7/79/Amul_Thapar.jpg"
+    },
+    {
+      name: "Don R. Willett",
+      id: 5846,
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Don_Willett_%287004180446%29.jpg/1920px-Don_Willett_%287004180446%29.jpg"
+    },
+    {
+      name: "Steven M. Colloton",
+      id: 686,
+      picture: "https://upload.wikimedia.org/wikipedia/commons/2/2d/Judge_Steven_Colloton_2016.jpg"
+    }
+  ]
+};
+
+export default Judges;
